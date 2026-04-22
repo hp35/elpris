8d35391534670f53212905c2c0efc14d02a6e0f5
Fredrik Jonsson <fredrik@jonsson.eu>
2026-04-22 17:47:51 +0200

    Adding colormap script.

---
+++ bash/colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Bash script for the generation of a table of available colors in
+# standard terminal output.
+#
+for x in {0..8}; do 
+    for i in {30..37}; do 
+        for a in {40..47}; do 
+            echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "
+        done
+        echo
+    done
+done
+echo ""
